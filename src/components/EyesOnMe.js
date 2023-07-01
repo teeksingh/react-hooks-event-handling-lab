00ae33d99bde36232039e43f7b13e12b931d3dd1
Prateek <teeksingh@gmail.com>
2023-06-30 21:45:44 -0500

    Complete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,9 @@
 // Code EyesOnMe Component Here
+function EyesOnMe () {
+    return (
+        <div>
+            <button onFocus={event => {console.log("Good!")}} onBlur={event => {console.log("Hey! Eyes on me!")}}>Eyes on me</button>
+        </div>
+    )
+}
+export default EyesOnMe
